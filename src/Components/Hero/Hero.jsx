3ed0dfa15f53983017c1d29a9095300841fde3c6
Vikash Kumar <vikashkumar6424@gmail.com>
2024-03-16 16:53:17 +0530

    Explore button

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Hero.css'
 import dark_arrow from '../../assets/dark-arrow.png'
 import msg_icon from '../../assets/msg-icon.png'
+import { Link } from 'react-scroll'
 
 const Hero = () => {
   return (
@@ -10,7 +11,7 @@ const Hero = () => {
       <h4>Admissions are open <img src={msg_icon} alt="" /></h4>
         <h1>ARIS-TOT-LERS </h1>
         <p>A Reggio Inspired School for Toddlers where Learning <br/> Enhances Reasoning</p>
-        <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button>
+       <Link to="programs" smooth={true} offset={-260} duration={500}> <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button></Link>
       </div>
     </div>
   )
