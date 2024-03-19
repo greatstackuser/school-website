4b7a1f680c3edff7f16e6cb7054451d5037a54c5
Vikash Kumar <vikashkumar6424@gmail.com>
2024-03-19 22:50:31 +0530

    about us

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -11,7 +11,7 @@ const About = ({setPlayState}) => {
         <img src={play_icon} alt="" className='play-icon' onClick={()=>{setPlayState(true)}}/>
       </div>
       <div className="about-right">
-        <h3>ABOUT SCHOOL</h3>
+        <h3>ABOUT US</h3>
         <h2>Nurturing Tomorrow's Leaders Today</h2>
         <p>At Aris-tot-lers Global Preschool, we are committed to providing a nurturing environment where learning enhances reasoning, thereby developing responsible and passionate citizens of the world.</p>
         <p>We provide a positive atmosphere where children can thrive and fulfil their potential thereby ensuring their academic and overall growth. We started out as a franchisee of Serra International Preschool in the year 2015 and after 4 years of successfully handling hundreds of children, we became Aris-tot-lers Global Preschool in 2019.</p>
