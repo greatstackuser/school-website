dfa505e5d939e26a09282fb827432badbfa737e2
Vikash Kumar <vikashkumar6424@gmail.com>
2024-03-20 14:18:43 +0530

    10 review

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,5 +1,9 @@
 import React, { useRef } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
 import './Testimonials.css'
+import 'swiper/css';
+import 'swiper/css/navigation';
 import next_icon from '../../assets/next-icon.png'
 import back_icon from '../../assets/back-icon.png'
 import user_1 from '../../assets/user-1.png'
@@ -9,29 +13,27 @@ import user_4 from '../../assets/user-4.png'
 
 const Testimonials = () => {
 
-    const slider = useRef();
-    let tx = 0;
 
-const slideForward = ()=>{
-    if(tx > -50){
-        tx -= 25;
-    }
-    slider.current.style.transform = `translateX(${tx}%)`;
-}
-const slideBackward = ()=>{
-    if(tx < 0){
-        tx += 25;
-    }
-    slider.current.style.transform = `translateX(${tx}%)`;
-}
+    const swiperRef = useRef();
 
   return (
+    
     <div className='testimonials' id='testimonials'>
-      <img src={next_icon} alt="" className='next-btn' onClick={slideForward}/>
-      <img src={back_icon} alt="" className='back-btn' onClick={slideBackward}/>
-      <div className="slider">
-        <ul ref={slider}>
-            <li>
+
+        <Swiper
+            slidesPerView='1'
+            breakpoints={{
+                700: { slidesPerView: 2},
+              }}
+            spaceBetween={0}
+            onBeforeInit={(swiper) => {
+                swiperRef.current = swiper;
+              }}
+            modules={[Navigation]}
+            className="mySwiper"
+        >
+            <SwiperSlide>
+           
                 <div className="slide">
                     <div className="user-info">
                         <img src={user_2} alt="" />
@@ -42,8 +44,10 @@ const slideBackward = ()=>{
                     </div>
                     <p>Perhaps the best pre school in the area. My daughter studied here from pre nursery till K2 learning a lot and loving every moment of it. Teachers very professional and knowledgeable and they also pay individual attention to all kids.</p>
                 </div>
-            </li>
-            <li>
+      
+            </SwiperSlide>
+            <SwiperSlide>
+              
                 <div className="slide">
                     <div className="user-info">
                         <img src={user_3} alt="" />
@@ -54,8 +58,10 @@ const slideBackward = ()=>{
                     </div>
                     <p>We are very happy to have selected Aristotlers for our kid (preschool and daycare) . The staff are very professional and take our feedback very seriously. Our kid looks forward to go to school everyday now.</p>
                 </div>
-            </li>
-            <li>
+         
+            </SwiperSlide>
+            <SwiperSlide>
+      
                 <div className="slide">
                     <div className="user-info">
                         <img src={user_4} alt="" />
@@ -66,8 +72,10 @@ const slideBackward = ()=>{
                     </div>
                     <p>The best school and daycare in town. The management and staff is very impressive and you can give the best start to your kid. It’s been 6 years now since we are associated with the school and daycare, and we are proud of that.</p>
                 </div>
-            </li>
-            <li>
+           
+            </SwiperSlide>
+            <SwiperSlide>
+            
                 <div className="slide">
                     <div className="user-info">
                         <img src={user_1} alt="" />
@@ -78,8 +86,10 @@ const slideBackward = ()=>{
                     </div>
                     <p>Wonderful pre-school. Teachers are very attentive to each kids’ needs. Overall it was very nice growth for my kid. He overcame many fears with help of teachers. He was unknown of English language but within few months started conversing in English very easily.</p>
                 </div>
-            </li>
-            {/* <li>
+            
+            </SwiperSlide>
+            <SwiperSlide>
+           
                 <div className="slide">
                     <div className="user-info">
                         <img src={user_1} alt="" />
@@ -90,11 +100,13 @@ const slideBackward = ()=>{
                     </div>
                     <p>I found Aristotlers five years back for my kid. I can say they have the best team of teachers in the school and daycare. Everyone is very approachable and the care taken to ensure that the kid is doing well is exceptional.</p>
                 </div>
-            </li>
-            <li>
+            
+            </SwiperSlide>
+            <SwiperSlide>
+         
                 <div className="slide">
                     <div className="user-info">
-                        <img src={user_1} alt="" />
+                        <img src={user_4} alt="" />
                         <div>
                             <h3>Kshitij Pande</h3>
                             <span>Brookefield </span>
@@ -102,8 +114,10 @@ const slideBackward = ()=>{
                     </div>
                     <p>One of the best preschools in AECS layout. I highly recommend this preschool. The management and the complete staff is too good. The teachers are skilled and overall development of the kids is taken care of. The teaching methodology is very good and organized at the same time. </p>
                 </div>
-            </li>
-            <li>
+         
+            </SwiperSlide>
+            <SwiperSlide>
+          
                 <div className="slide">
                     <div className="user-info">
                         <img src={user_1} alt="" />
@@ -114,11 +128,13 @@ const slideBackward = ()=>{
                     </div>
                     <p>My kid attended Aristotlers for four years, all the way from playgroup to ukg, and loved it! The teachers and staff are kind and caring. Vibha (who runs Aristotlers) is always available to talk, and actually listens to parents. She knew every child at Aristotlers.</p>
                 </div>
-            </li>
-            <li>
+           
+            </SwiperSlide>
+            <SwiperSlide>
+
                 <div className="slide">
                     <div className="user-info">
-                        <img src={user_1} alt="" />
+                        <img src={user_4} alt="" />
                         <div>
                             <h3>Ankur Kansal</h3>
                             <span>Brookefield </span>
@@ -126,8 +142,10 @@ const slideBackward = ()=>{
                     </div>
                     <p>On 1st March I attended aris-tot-lers annual day function. Seeing my kid on stage first time performing was altogether a different experience.I can't imagine how much effort team has put in organizing this event and practicing toddlers to perform on the stage infront of such large audience. Kudos to Vibha And her team.</p>
                 </div>
-            </li>
-            <li>
+       
+            </SwiperSlide>
+            <SwiperSlide>
+         
                 <div className="slide">
                     <div className="user-info">
                         <img src={user_1} alt="" />
@@ -138,8 +156,10 @@ const slideBackward = ()=>{
                     </div>
                     <p>If somebody is looking to put their kid in a play / pre-primary school during his or her early years, it is a wonderful place. The staff, teachers and management are great! Teachers are really skilled. I am grateful and thankful for the kind of experience that my child had during his time over there.</p>
                 </div>
-            </li>
-            <li>
+         
+            </SwiperSlide>
+            <SwiperSlide>
+       
                 <div className="slide">
                     <div className="user-info">
                         <img src={user_1} alt="" />
@@ -150,10 +170,17 @@ const slideBackward = ()=>{
                     </div>
                     <p>Nice place to send your kids to. My daughter joined when she was 2.2 years old. She loves going to school!! She’s learnt alot in one year. Whats most important is that she’s happy going to and being in the school. Teachers and support staff are good and caring.</p>
                 </div>
-            </li> */}
-        </ul>
-      </div>
+           
+            </SwiperSlide>
+
+            
+        </Swiper>
+
+        <img src={next_icon} alt="" className='next-btn' onClick={() => swiperRef.current?.slideNext()}/>
+        <img src={back_icon} alt="" className='back-btn' onClick={() => swiperRef.current?.slidePrev()}/>
+      
     </div>
+
   )
 }
 
