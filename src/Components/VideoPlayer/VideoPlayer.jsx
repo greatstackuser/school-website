ff271ec2b4c70029bc0ec670dc041de27e97ea46
Vikash Kumar <vikashkumar6424@gmail.com>
2024-03-24 20:31:27 +0530

    video fix

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -5,16 +5,24 @@ import video from '../../assets/college-video.mp4'
 const VideoPlayer = ({playState, setPlayState}) => {
 
     const player = useRef(null);
+    const vid = useRef(null);
+
 
     const closePlayer = (e)=>{
         if(e.target === player.current){
            setPlayState(false); 
         }
+
     }
+
+    useEffect(()=>{
+      playState?vid.current.play():vid.current.pause()
+    },[playState])
+
 
   return (
     <div className={`video-player ${playState?'hide':''}`} ref={player} onClick={closePlayer}>
-      <video muted controls>
+      <video controls ref={vid}>
        <source src={video}/> 
       </video>
     </div>
